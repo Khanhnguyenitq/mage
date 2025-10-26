ad4afaed1070613f96ec08ca7ac0b1e31e85aeaf
root <root@acer>
2025-10-26 23:41:52 +0700

    2

--- app/design/frontend/TestMage/mytheme/Magento_Swatches/web/js/swatch-renderer-mixin.js
+++ app/design/frontend/TestMage/mytheme/Magento_Swatches/web/js/swatch-renderer-mixin.js
@@ -6,7 +6,7 @@ define([
 
     return function (widget) {
         $.widget('mage.SwatchRenderer', widget, {
-            
+
             /**
              * Override _RenderSwatchOptions - chỉ custom image type
              */
@@ -43,21 +43,21 @@ define([
 
                     id = this.id;
                     type = parseInt(optionConfig[id].type, 10);
-                    value = optionConfig[id].hasOwnProperty('value') ? 
-                            optionConfig[id].value : '';
-                    thumb = optionConfig[id].hasOwnProperty('thumb') ? 
-                            optionConfig[id].thumb : '';
+                    value = optionConfig[id].hasOwnProperty('value') ?
+                        optionConfig[id].value : '';
+                    thumb = optionConfig[id].hasOwnProperty('thumb') ?
+                        optionConfig[id].thumb : '';
                     label = this.label ? this.label : '';
                     attr = ' id="' + controlId + '-item-' + id + '"' +
-                           ' aria-label="' + label + '"' +
-                           ' index="' + index + '"' +
-                           ' option-type="' + type + '"' +
-                           ' option-id="' + id + '"' +
-                           ' option-label="' + label + '"' +
-                           ' aria-checked="false"' +
-                           ' role="option"' +
-                           ' tabindex="0"' +
-                           ' option-tooltip-value=""';
+                        ' aria-label="' + label + '"' +
+                        ' index="' + index + '"' +
+                        ' option-type="' + type + '"' +
+                        ' option-id="' + id + '"' +
+                        ' option-label="' + label + '"' +
+                        ' aria-checked="false"' +
+                        ' role="option"' +
+                        ' tabindex="0"' +
+                        ' option-tooltip-value=""';
 
                     if (!this.hasOwnProperty('products') || this.products.length <= 0) {
                         attr += ' option-empty="true"';
@@ -70,29 +70,29 @@ define([
                         // Image swatch - style dạng card
                         swatchImageWidth = 150;
                         swatchImageHeight = 150;
-                        
+
                         html += '<div class="' + optionClass + ' image" ' + attr + '>' +
-                                '<div class="swatch-image-card">' +
-                                '<div class="swatch-image-wrapper">' +
-                                '<img src="' + value + '" ' +
-                                'alt="' + label + '" ' +
-                                'width="' + swatchImageWidth + '" ' +
-                                'height="' + swatchImageHeight + '">' +
-                                '</div>' +
-                                '<div class="swatch-image-label">' + label + '</div>' +
-                                '</div>' +
-                                '</div>';
-                    } 
+                            '<div class="swatch-image-card">' +
+                            '<div class="swatch-image-label">' + label + '</div>' +
+                            '<div class="swatch-image-wrapper">' +
+                            '<img src="' + value + '" ' +
+                            'alt="' + label + '" ' +
+                            'width="' + swatchImageWidth + '" ' +
+                            'height="' + swatchImageHeight + '">' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>';
+                    }
                     // TEXT & COLOR GIỮ NGUYÊN STYLE GỐC
                     else if (type === 0) {
                         // Text swatch - default style
                         html += '<div class="' + optionClass + ' text" ' + attr + '>' +
-                                (value ? value : label) +
-                                '</div>';
+                            (value ? value : label) +
+                            '</div>';
                     } else if (type === 1) {
                         // Color swatch - default style
                         html += '<div class="' + optionClass + ' color" ' + attr +
-                                ' style="background: ' + value + ' no-repeat center;"></div>';
+                            ' style="background: ' + value + ' no-repeat center;"></div>';
                     } else if (type === 3) {
                         // Empty swatch
                         html += '<div class="' + optionClass + '" ' + attr + '></div>';
@@ -121,6 +121,7 @@ define([
             /**
              * Initialize Swiper
              */
+
             _initSwiper: function () {
                 var self = this;
 
@@ -136,37 +137,81 @@ define([
                     var $swiper = $attribute.find('.swiper');
 
                     if ($swiper.length && !$swiper.hasClass('swiper-initialized')) {
+
+                        // ĐẾM số options
+                        var totalSlides = $swiper.find('.swiper-slide').length;
+
                         var swiperInstance = new Swiper($swiper[0], {
                             slidesPerView: 'auto',
                             spaceBetween: 15,
-                            navigation: {
-                                nextEl: $swiper.find('.swiper-button-next')[0],
-                                prevEl: $swiper.find('.swiper-button-prev')[0],
+
+                            // QUAN TRỌNG: Enable các tính năng scroll/swipe
+                            freeMode: {
+                                enabled: true,
+                                sticky: false,
+                                momentumBounce: false
                             },
+
+                            // Enable mouse wheel scroll
+                            mousewheel: {
+                                enabled: true,
+                                forceToAxis: true
+                            },
+
+                            // Cho phép kéo bằng chuột
+                            simulateTouch: true,
+                            grabCursor: true,
+                            touchRatio: 1,
+                            touchAngle: 45,
+
+                            // Không tự động center
+                            centeredSlides: false,
+
+                            // Loopable nếu nhiều slides
+                            loop: false,
+
+                            // Responsive
                             breakpoints: {
                                 320: {
-                                    slidesPerView: 2,
-                                    spaceBetween: 10
+                                    slidesPerView: 'auto',
+                                    spaceBetween: 10,
+                                    slidesPerGroup: 1
                                 },
                                 640: {
-                                    slidesPerView: 3,
-                                    spaceBetween: 12
+                                    slidesPerView: 'auto',
+                                    spaceBetween: 12,
+                                    slidesPerGroup: 2
                                 },
                                 768: {
-                                    slidesPerView: 4,
-                                    spaceBetween: 15
+                                    slidesPerView: 'auto',
+                                    spaceBetween: 15,
+                                    slidesPerGroup: 3
                                 },
                                 1024: {
-                                    slidesPerView: 5,
-                                    spaceBetween: 15
+                                    slidesPerView: 'auto',
+                                    spaceBetween: 15,
+                                    slidesPerGroup: 4
                                 }
                             },
+
                             watchOverflow: true,
                             observer: true,
-                            observeParents: true
+                            observeParents: true,
+                            observeSlideChildren: true,
+
+                            // Callback sau khi init
+                            on: {
+                                init: function () {
+                                    console.log('Swiper initialized with ' + totalSlides + ' slides');
+                                    this.update();
+                                }
+                            }
                         });
 
                         $attribute.data('swiper-instance', swiperInstance);
+
+                        // Debug: Log ra để check
+                        console.log('Swiper instance created:', swiperInstance);
                     }
                 });
             },
@@ -176,7 +221,7 @@ define([
              */
             _Rebuild: function () {
                 var $widget = this;
-                
+
                 // Destroy swiper cũ
                 this.element.find('.swatch-attribute').each(function () {
                     var instance = $(this).data('swiper-instance');
@@ -190,7 +235,7 @@ define([
                 this._super();
 
                 // Init lại swiper
-                setTimeout(function() {
+                setTimeout(function () {
                     $widget._initSwiper();
                 }, 100);
             }
