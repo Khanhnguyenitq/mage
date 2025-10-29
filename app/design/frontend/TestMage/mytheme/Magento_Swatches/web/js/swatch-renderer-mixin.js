fb456a984367ff7122b654c12bda4b909f2909b7
root <root@acer>
2025-10-30 00:55:52 +0700

    new

--- app/design/frontend/TestMage/mytheme/Magento_Swatches/web/js/swatch-renderer-mixin.js
+++ app/design/frontend/TestMage/mytheme/Magento_Swatches/web/js/swatch-renderer-mixin.js
@@ -1,6 +1,3 @@
-/**
- * Swatch Slider Mixin
- */
 define(['jquery', 'swiper'], function ($, Swiper) {
     'use strict';
 
@@ -15,13 +12,13 @@ define(['jquery', 'swiper'], function ($, Swiper) {
 
                 if (optionConfig) {
                     $.each(optionConfig, function (key, value) {
-                        if (parseInt(value.type, 10) === 2) { // type 2 = image
+                        if (parseInt(value.type, 10) === 2) {
                             hasImageSwatches = true;
-                            return false; // break loop
+                            return false;
                         }
                     });
                 }
-                  if (!hasImageSwatches) {
+                if (!hasImageSwatches) {
                     return html;
                 }
 
@@ -36,7 +33,6 @@ define(['jquery', 'swiper'], function ($, Swiper) {
                 wrappedHtml += '</div>';
                 wrappedHtml += '</div></div>';
 
-                // Init swiper
                 setTimeout(function () {
                     new Swiper('.' + sliderId, {
                         slidesPerView: 'auto',
@@ -48,6 +44,69 @@ define(['jquery', 'swiper'], function ($, Swiper) {
                 }, 100);
 
                 return wrappedHtml;
+            },
+            _RenderSwatchSelect: function (config, chooseText) {
+                var html = this._super(config, chooseText);
+
+                if (!this.options.jsonSwatchConfig.hasOwnProperty(config.id)) {
+                    return html;
+                }
+
+                return html;
+            },
+            _EventListener: function () {
+                var $widget = this;
+                this._super();
+                this._AddRadioIcons();
+            },
+
+            _AddRadioIcons: function () {
+                var $widget = this;
+
+                this.element.find('.swatch-attribute').each(function () {
+                    var $attribute = $(this),
+                        attributeId = $attribute.attr('data-attribute-id'),
+                        isSwatchConfig = $widget.options.jsonSwatchConfig.hasOwnProperty(attributeId);
+
+                    if (!isSwatchConfig || $attribute.find('.swatch-option.image').length === 0) {
+                        $attribute.find('.swatch-option.text, .swatch-option.color').each(function () {
+                            var $option = $(this);
+
+                            if ($option.find('.radio-icon').length === 0) {
+                                $option.prepend('<span class="radio-icon"></span>');
+                                $option.addClass('with-radio-icon');
+                            }
+                        });
+                    }
+                });
+            },
+
+            _OnClick: function ($this, $widget) {
+                var $parent = $this.closest('.swatch-attribute'),
+                    attributeId = $parent.data('attribute-id'),
+                    $input = $parent.find('.super-attribute-select');
+
+                if ($this.hasClass('selected')) {
+                    $this.removeClass('selected');
+                    $parent.removeAttr('data-option-selected');
+
+                    $input.val('').trigger('change');
+
+                    return;
+                }
+
+                this._super($this, $widget);
+
+                if ($this.hasClass('with-radio-icon')) {
+                    $this.siblings('.with-radio-icon').removeClass('selected');
+                    $this.addClass('selected');
+                }
+            },
+
+
+            _Rebuild: function () {
+                this._super();
+                this._AddRadioIcons();
             }
         });
 
